e1c630b35a19ed0661b3ba94ee00c8a0ac8280c7
Rocket Search <42518743+Rocket-Search@users.noreply.github.com>
2025-06-19 10:49:41 +0200

    bug fixing S32 datatype convertion from 16 bit
    bug fixing S32 datatype convertion from 16 bit

--- sigenergy_modbus.cpp
+++ sigenergy_modbus.cpp
@@ -1,6 +1,8 @@
-//compile	#g++ -g -O3 -fPIC -Wall -std=c++20 -fpermissive -c -o sigenergy_modbus.o sigenergy_modbus.cpp; 
-//link		#g++ -O3 -fPIC -Wall -o sigenergy_modbus sigenergy_modbus.o -lmodbus;
-//Execute	#./sigenergy_modbus config_sigenergy_register_addresse_slave_247.cfg 247 192.168.188.69 502
+//complie
+//g++ -g -O3 -fPIC -Wall -std=c++20 -fpermissive -c -o sigenergy_modbus.o sigenergy_modbus.cpp;
+//link
+//g++ -O3 -fPIC -Wall -o sigenergy_modbus sigenergy_modbus.o -lmodbus
+//Execute# ./sigenergy_modbus config_sigenergy_register_addresse_slave_247.cfg 247 192.168.188.69 502
 
 
 #include <stdio.h>
@@ -64,24 +66,24 @@ int main(int argc, char *argv[])
 	int register_anzahl_int = -1;
 	int wo_ist_semikolon = - 1;
 
-
-
+	
+	
 	string ip = argv[3];
 	string port = argv[4];
 	int port_int = -1;
 	int rc = -1;
 	
-
-
-
+	
+	
+	
 	port_int = stoi(port);
 	modbus_t *ctx = modbus_new_tcp(ip.c_str(), port_int);
 		
-
+	
 	string slave_address = argv[2];
 	int slave_address_int = -1;
 	slave_address_int = stoi(slave_address);
-
+	
 	rc = modbus_set_slave(ctx, slave_address_int);
 	
 	if (rc == -1) 
@@ -101,6 +103,7 @@ int main(int argc, char *argv[])
 	uint16_t tab_reg_u32[32];
 	uint32_t tab_reg_real_u32;
 	int16_t tab_reg_s32[32];
+	int32_t tab_reg_real_s32;
 	int16_t tab_reg_s16[16];
 	char tab_reg_string_real[65];
 	uint16_t tab_reg_u64[64];
@@ -110,94 +113,102 @@ int main(int argc, char *argv[])
 
 	for (int i = 1; i < config_zeile.size(); i++)
 	{
-
-
 		
-
-
+		
+		
+		
+		
 		config_zeile_temp = config_zeile[i];
 		wo_ist_semikolon = config_zeile_temp.find(";");
 		register_beschreibung = config_zeile_temp.substr(0,wo_ist_semikolon);
 		register_beschreibung.erase(std::remove(register_beschreibung.begin(), register_beschreibung.end(), '\t'), register_beschreibung.end());
-
-		config_zeile_temp = config_zeile_temp.substr(wo_ist_semikolon + 1, config_zeile_temp.size());
-
 		
-
+		config_zeile_temp = config_zeile_temp.substr(wo_ist_semikolon + 1, config_zeile_temp.size());
+		
+		
+		
 		wo_ist_semikolon = config_zeile_temp.find(";");
 		register_adresse = config_zeile_temp.substr(0,wo_ist_semikolon);
 		register_adresse.erase(std::remove(register_adresse.begin(), register_adresse.end(), '\t'), register_adresse.end());
-
+		
 		config_zeile_temp = config_zeile_temp.substr(wo_ist_semikolon + 1, config_zeile_temp.size());
-
-
+		
 		register_adresse_int=stoi(register_adresse);
 		
-
+		
 		wo_ist_semikolon = config_zeile_temp.find(";");
 		register_anzahl = config_zeile_temp.substr(0,wo_ist_semikolon);
 		register_anzahl.erase(std::remove(register_anzahl.begin(), register_anzahl.end(), '\t'), register_anzahl.end());
-
+		
 		config_zeile_temp = config_zeile_temp.substr(wo_ist_semikolon + 1, config_zeile_temp.size());
-
+		
 		register_anzahl_int = stoi(register_anzahl);
 		
-
+		
 		wo_ist_semikolon = config_zeile_temp.find(";");
 		register_data_type = config_zeile_temp.substr(0,wo_ist_semikolon);
 		register_data_type.erase(std::remove(register_data_type.begin(), register_data_type.end(), '\t'), register_data_type.end());
-
+		
 		config_zeile_temp = config_zeile_temp.substr(wo_ist_semikolon + 1, config_zeile_temp.size());
-
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		cout<<"#################################################"<<endl;
-
+		
 		if (register_data_type == "S16")
 		{
 			rc = modbus_read_input_registers(ctx, register_adresse_int,register_anzahl_int , tab_reg_s16); 
 			if (rc == -1) 
 			{
 				cout<<"#reg addr.#"<<register_adresse_int<<"#read error#"<<modbus_strerror(errno)<<"#"<<endl;
-
+		
 			}
 			for (int i=0; i < rc; i++) 
 			{
-
+		
 				cout<<register_beschreibung<<"#"<<tab_reg_s16[i]<<"#"<<endl;
 			}
 		}
-
+		
 		if (register_data_type == "S32")
 		{
 			rc = modbus_read_input_registers(ctx, register_adresse_int,register_anzahl_int , tab_reg_s32); 
 			if (rc == -1) 
 			{
-
+		
 				cout<<"#reg addr.#"<<register_adresse_int<<"#read error#"<<modbus_strerror(errno)<<"#"<<endl;
-
+		
 			}
 			for (int i=0; i < rc; i++) 
 			{
-
-				cout<<register_beschreibung<<"#"<<tab_reg_s32[i]<<"#"<<endl;
-
+		
+		
+				tab_reg_real_s32 = (((int32_t)tab_reg_s32[i] << 16) | (int32_t)tab_reg_s32[i+1]);
+				cout<<register_beschreibung<<"#"<<tab_reg_real_s32<<"#"<<endl;
+		
 			}
 		}
-
+		
 		if (register_data_type == "U32")
 		{
 			rc = modbus_read_input_registers(ctx, register_adresse_int,register_anzahl_int , tab_reg_u32); 
 			if (rc == -1) 
 			{
-
+				
 				cout<<"#reg addr.#"<<register_adresse_int<<"#read error#"<<modbus_strerror(errno)<<"#"<<endl;
-
+				
 			}
 			for (int i=0; i < rc; i++) 
 			{
-
-
+				
+				
 				tab_reg_real_u32 = (((uint32_t)tab_reg_u32[i] << 16) | (uint32_t)tab_reg_u32[i+1]);
 
 				cout<<register_beschreibung<<"#"<<tab_reg_real_u32<<"#"<<endl;
@@ -254,8 +265,7 @@ int main(int argc, char *argv[])
 			for (int i=0; i < rc; i++) 
 			{
 
-				tab_reg_real_u64 = MODBUS_GET_INT64_FROM_INT16(tab_reg_u64, 0 );
-
+				tab_reg_real_u64 = MODBUS_GET_INT64_FROM_INT16(tab_reg_u64, 0 );							
 
 				cout<<register_beschreibung<<"#"<<tab_reg_real_u64<<"#"<<endl;
 			}
